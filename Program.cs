09b5373ce69ce383e3d3439562177935585e2c2d
Pavel Linkevich <pavellinkrvich2003@gmail.com>
2019-07-14 13:31:37 +0300

    Исправил ошибку

--- Program.cs
+++ Program.cs
@@ -52,6 +52,60 @@ namespace TicTacToe
         {
 
         }
+        static void Twoplayers()
+        {
+          
+            while (Win() ==true || Win2() == true)
+            {
+                Move1(field);
+                if (Win() == false || Win2() == false)
+                {
+                    break;
+                }
+                Move2(field);
+                if (Win() == false || Win2() == false)
+                {
+                    break;
+                }
+            }
+        }
+        static void Move1(char[,] field)//Доработаю
+        {
+            Console.WriteLine("ход 1 игрока");
+            Console.WriteLine("укажите столбик:");
+            int move2 = Parse()-1;
+
+            Console.WriteLine("укажите ряд:");
+            int move = Parse()-1;
+
+            field[move, move2] = 'X';
+            Move(field);
+        }
+        static void Move(char[,] field)//Доработаю
+        {                        
+            for (int t = 0; t < a; t++)
+            {
+                Console.Write(" |");
+
+                for (int y = 0; y < b; y++)
+                {
+                    Console.Write(field[t, y] + "|");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Move2(char[,] field)//Доработаю
+        {
+            Console.WriteLine("ход 2 игрока");
+            Console.WriteLine("укажите столбик:");
+            int move2 = Parse() - 1;
+            Console.WriteLine("укажите ряд:");
+            int move = Parse() - 1;
+
+            field[move, move2] = 'O';
+            Move(field);
+        }
         static bool Win()
         {
 
@@ -93,14 +147,14 @@ namespace TicTacToe
             {
                 for (int p = 0; p < b; p++)
                 {
-                    if (field[p, i - 2] == 'O' && field[p, i - 1] == 'O' && field[p, i] == 'O') { Console.WriteLine("Победа 1 игрока"); return false; }// 3 в ряд
+                    if (field[p, i - 2] == 'O' && field[p, i - 1] == 'O' && field[p, i] == 'O') { Console.WriteLine("Победа 2 игрока"); return false; }// 3 в ряд
                 }
             }
             for (int i = 2; i < a; i++)
             {
                 for (int p = 0; p < b; p++)
                 {
-                    if (field[i - 2, p] == 'O' && field[i - 1, p] == 'O' && field[i, p] == 'O') { Console.WriteLine("Победа 1 игрока"); return false; }// 3  по вертикали
+                    if (field[i - 2, p] == 'O' && field[i - 1, p] == 'O' && field[i, p] == 'O') { Console.WriteLine("Победа 2 игрока"); return false; }// 3  по вертикали
                 }
             }
 
@@ -108,66 +162,17 @@ namespace TicTacToe
             {
                 for (int p = 2; p < b; p++)
                 {
-                    if (field[i - 2, p - 2] == 'O' && field[i - 1, p - 1] == 'O' && field[i, p] == 'O') { Console.WriteLine("Победа 1 игрока"); return false; }// 3  по диаганали
+                    if (field[i - 2, p - 2] == 'O' && field[i - 1, p - 1] == 'O' && field[i, p] == 'O') { Console.WriteLine("Победа 2 игрока"); return false; }// 3  по диаганали
                 }
             }
             for (int i = 2; i < a; i++)
             {
                 for (int p = 2; p < b; p++)
                 {
-                    if (field[p - 2, i] == 'O' && field[p - 1, i - 1] == 'O' && field[p, i - 2] == 'O') { Console.WriteLine("Победа 1 игрока"); return false; }
+                    if (field[p - 2, i] == 'O' && field[p - 1, i - 1] == 'O' && field[p, i - 2] == 'O') { Console.WriteLine("Победа 2 игрока"); return false; }
                 }
             }
             return true;
         }
-        static void Twoplayers()
-        {
-            while (Win()!= false || Win2() != false)
-            {                
-                Move1(field);
-                if (Win() != false || Win2() != false)
-                {
-                    break;
-                }
-                Move2(field);
-            }
-        }
-        static void Move1(char[,] field)//Доработаю
-        {
-            Console.WriteLine("ход 1 игрока");
-            Console.WriteLine("укажите столбик:");
-            int move2 = Parse()-1;
-            Console.WriteLine("укажите ряд:");
-            int move = Parse()-1;
-
-            field[move, move2] = 'X';
-            Move(field);
-        }
-        static void Move(char[,] field)//Доработаю
-        {                        
-            for (int t = 0; t < a; t++)
-            {
-                Console.Write(" |");
-
-                for (int y = 0; y < b; y++)
-                {
-                    Console.Write(field[t, y] + "|");
-                }
-                Console.WriteLine();
-            }
-        }
-
-        static void Move2(char[,] field)//Доработаю
-        {
-            Console.WriteLine("ход 2 игрока");
-            Console.WriteLine("укажите столбик:");
-            int move2 = Parse() - 1;
-            Console.WriteLine("укажите ряд:");
-            int move = Parse() - 1;
-
-            field[move, move2] = 'O';
-            Move(field);
-        }
-
     }
 }
